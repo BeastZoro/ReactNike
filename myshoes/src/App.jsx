4cf0ce1c50dc9def82b5cbf17972b95ef6cb8e0c
BeastZoro <126886437+BeastZoro@users.noreply.github.com>
2023-09-30 18:35:15 +0530

    Initial commit

---
+++ myshoes/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+const App = () => {
+  return (
+    <main className="relative">
+     Nav
+      <section className="xl:padding-1 wide:padding-right padding-bottom">
+      Hero
+      </section>
+
+      <section className="">
+        
+      </section>
+    </main>
+  )
+}
+
+export default App
