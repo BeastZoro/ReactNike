e00e68067cee06837f3827bea52a249ac60d1087
BeastZoro <126886437+BeastZoro@users.noreply.github.com>
2023-10-06 18:39:18 +0530

    multiple sections

--- myshoes/src/sections/CustomerReviews.jsx
+++ myshoes/src/sections/CustomerReviews.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import { reviews } from "../constants";
+import { star } from "../assets/icons";
 
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
-  )
-}
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold">
+        What Our <span className="text-coral-red">Customers</span> Say?
+      </h3>
+      <p className="mt-4 m-auto max-w-lg text-center info-text">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque,
+        facilis.
+      </p>
 
-export default CustomerReviews
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review, index) => (
+          <ReviewCard key={index} review={review} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default CustomerReviews;
+
+const ReviewCard = ({ review }) => {
+  const { imgURL, customerName, rating, feedback } = review;
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <img
+        src={imgURL}
+        alt="customer"
+        className="rounded-full object-cover w-[120px] h-[120px]"
+      />
+      <p className="mt-6 max-w-sm text-center info-text">{feedback}</p>
+      <div className="mt-3 flex justify-center items-center gap-2.5">
+        <img
+          src={star}
+          alt="rating star"
+          width={24}
+          height={24}
+          className="object-contain m-0"
+        />
+        <p className="text-xl font-montserrat text-slate-gray">({rating})</p>
+        <h3 className="mt-1 font-palanquin text-3xl text-center font-bold">{customerName}</h3>
+      </div>
+    </div>
+  );
+};
--- myshoes/src/sections/SpecialOffer.jsx
+++ myshoes/src/sections/SpecialOffer.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from "react";
+import { offer } from "../assets/images";
+import Button from "../Components/Button";
+import { arrowRight } from "../assets/icons";
 
 const SpecialOffer = () => {
   return (
-    <section>
-      
+    <section className="flex justify-between items-center max-xl:flex-col-reverse gap-10 max-container">
+      <div className="flex-1">
+        <img
+          src={offer}
+          alt="shoe offer"
+          width={773}
+          height={687}
+          className="object-contain w-full"
+        />
+      </div>
+      <div className="flex flex-1 flex-col">
+        <h2 className="text-4xl font-palanquin font-bold">
+          <span className="text-coral-red">Special</span> Offer
+        </h2>
+        <p className="mt-4 info-text">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero saepe
+          animi quod officia dolor dolorem voluptates aliquid quia,
+          exercitationem ea, rerum autem dignissimos minus? Voluptas nesciunt
+          harum odio aperiam dignissimos!
+        </p>
+        <p className="mt-6 info-text">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem dolores placeat quo aperiam debitis optio esse expedita
+          id, laboriosam cupiditate?
+        </p>
+        <div className="mt-11 flex flex-wrap gap-4">
+          <Button label={"Shop now"} iconURL={arrowRight} />
+          <Button
+            label={"Learn More"}
+            backgroundColor="bg-white"
+            borderColor="border-slate-gray"
+            textColor="text-slate-gray"
+          />
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default SpecialOffer
+export default SpecialOffer;
